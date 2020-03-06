d3a76f40d00e64d28f3d94ffbe5b158199dda2a1
David Y. Stephenson <david@davidystephenson.com>
2020-03-06 10:35:58 +0100

    Register POST /message

--- index.js
+++ index.js
@@ -4,6 +4,26 @@ const app = express()
 
 const port = 4000
 
+const db = {}
+
+db.messages = []
+
+const parser = express.json()
+app.use(parser)
+
+app.post(
+  '/message',
+  (request, response) => {
+    const { text } = request.body
+
+    db.messages.push(text)
+
+    response.send(text)
+
+    console.log('db test:', db)
+  }
+)
+
 app.listen(
   port,
   () => console.log(`Listening on :${port}`)
